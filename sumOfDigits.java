0d17198ab5dfb72ced832853d9bdad6ca9cdbece
NIKESH <nikeshnoida@gmail.com>
2025-03-10 21:26:23 +0530

    Add files via upload

---
+++ sumOfDigits.java
@@ -0,0 +1,15 @@
+package Conditional;
+import java.util.Scanner;
+public class sumOfDigits {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.printf("enter the integer");
+        int num= input.nextInt();
+        int sum = 0;
+        while(num>0){
+            sum = sum+(num%10);
+            num = num/10;
+        }
+        System.out.printf("the sum is "+sum);
+    }
+}
