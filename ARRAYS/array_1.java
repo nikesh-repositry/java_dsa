657ee6ab8d2445166f46b05d7fee081c2fe3cc3e
NIKESH <nikeshnoida@gmail.com>
2025-05-15 22:46:50 +0530

    Add files via upload

---
+++ ARRAYS/array_1.java
@@ -0,0 +1,18 @@
+package dsa;
+
+public class array_1 {
+    public static void main(String[] args) {
+        //declaration only
+        int[] num1;
+
+        //declaration and instantiation
+        int[] num2 = new int[10];
+
+        //declaration and instantiation and initialization with values
+        int[] num3 = {1,2,3,4,5};
+
+        //declaration and then instantiation
+        int[] num4;
+        num4 = new int[5];
+    }
+}
